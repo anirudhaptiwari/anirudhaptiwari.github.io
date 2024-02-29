7760cb8c34763a1edeb600fab7fb460b82fc4e8b
Anirudha Pradumna Tiwari <114850796+anirudhaptiwari@users.noreply.github.com>
2024-02-29 18:39:41 +0530

    Update cli.js

--- src/cli.js
+++ src/cli.js
@@ -15,7 +15,7 @@ const struct = {
 
 const commands = {};
 let systemData = {};
-const rootPath = 'users/anirudhaptiwari/root';
+const rootPath = 'users/codebytere/root';
 
 const getDirectory = () => localStorage.directory;
 const setDirectory = (dir) => {
